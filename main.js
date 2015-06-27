f38c4514f1231224f42aa714aec86df389a19502
Larry Zhang <lzhang124@gmail.com>
2015-06-26 20:44:17 -0600

    added array and click functionality

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 var app = angular.module('calendar', []);
 
 app.controller('calendarController', function($scope) {
-  $scope.hours = 24;
-  $scope.days = 7;
+  var hours = 24;
+  var days = 7;
+  $scope.schedule = new Array(hours);
+  for (var i = 0; i < hours; i++) {
+    $scope.schedule[i] = new Array(days);
+  }
+
+  $scope.number = function(num) {
+    return new Array(num);
+  }
+
+  $scope.select = function(day, hour) {
+    if ($scope.schedule[hour][day] === 1) {
+      $scope.schedule[hour][day] = null;
+    } else {
+      $scope.schedule[hour][day] = 1;
+    }
+    console.log('day='+day, 'hour='+hour);
+    console.log($scope.schedule[hour][day] === 1);
+  }
 });
