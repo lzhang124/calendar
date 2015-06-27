ba04fc7a336b6fa807d53a03a7331717260c8ab1
Larry Zhang <lzhang124@gmail.com>
2015-06-27 01:18:09 -0600

    added click and drag

--- main.js
+++ main.js
@@ -47,18 +47,19 @@ app.controller('calendarController', function($scope) {
     return new Array(num);
   }
 
-  $scope.select = function(day, hour) {
-    if ($scope.schedule[day][hour] === 1) {
-      $scope.schedule[day][hour] = null;
-    } else {
-      $scope.schedule[day][hour] = 1;
-    }
-    console.log('day='+day, 'hour='+hour);
-    scheduleTimes();
-  }
+  // $scope.select = function(day, hour) {
+  //   if ($scope.schedule[day][hour] === 1) {
+  //     $scope.schedule[day][hour] = null;
+  //   } else {
+  //     $scope.schedule[day][hour] = 1;
+  //   }
+  //   console.log('day='+day, 'hour='+hour);
+  //   scheduleTimes();
+  // }
 
   $scope.startTimes = [];
   $scope.durations = [];
+
   var scheduleTimes = function() {
     $scope.startTimes = [];
     $scope.durations = [];
@@ -81,6 +82,85 @@ app.controller('calendarController', function($scope) {
           }
         }
       }
+    }
+  }
+});
+
+app.directive('dragSelect', function($window, $document) {
+  return {
+    controller: function($scope, $element) {
+      var startCell = null;
+      var dragging = false;
+
+      function mouseUp(el) {
+        dragging = false;
+      }
+      
+      function mouseDown(el) {
+        dragging = true;
+        setStartCell(el);
+        setEndCell(el);
+      }
+      
+      function mouseEnter(el) {
+        if (!dragging) return;
+        setEndCell(el);
+      }
+      
+      function setStartCell(el) {
+        startCell = el;
+      }
+      
+      function setEndCell(el) {
+        $element.find($('.c')).removeClass('selected');
+        cellsBetween(startCell, el).each(function() {
+          var el = angular.element(this);
+          el.addClass('selected');
+        });
+      }
+      
+      function cellsBetween(start, end) {
+        var coordsStart = getCoords(start);
+        var coordsEnd = getCoords(end);
+        var topLeft = {
+          column: $window.Math.min(coordsStart.column, coordsEnd.column),
+          row: $window.Math.min(coordsStart.row, coordsEnd.row),
+        };
+        var bottomRight = {
+          column: $window.Math.max(coordsStart.column, coordsEnd.column),
+          row: $window.Math.max(coordsStart.row, coordsEnd.row),
+        };
+        return $element.find($('.c')).filter(function() {
+          var el = angular.element(this);
+          var coords = getCoords(el);
+          return coords.column >= topLeft.column
+              && coords.column <= bottomRight.column
+              && coords.row >= topLeft.row
+              && coords.row <= bottomRight.row;
+        });
+      }
+      
+      function getCoords(cell) {
+        var row = cell.parents('row');
+        console.log(cell[0].cellIndex);
+        return {
+          column: cell[0].cellIndex, 
+          row: cell.parent()[0].rowIndex
+        };
+      }
+    
+      function wrap(fn) {
+        return function() {
+          var el = angular.element(this);
+          $scope.$apply(function() {
+            fn(el);
+          });
+        }
+      }
+      
+      $element.delegate('.c', 'mousedown', wrap(mouseDown));
+      $element.delegate('.c', 'mouseenter', wrap(mouseEnter));
+      $document.delegate('body', 'mouseup', wrap(mouseUp));
     }
   }
 });
